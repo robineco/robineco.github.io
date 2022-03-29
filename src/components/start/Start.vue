6253bb69ff0e895c374c899701f78e68202501ed
robineco <robin.schlund@tutanota.de>
2022-03-29 11:42:48 +0200

    added quick links

--- src/components/start/Start.vue
+++ src/components/start/Start.vue
@@ -6,6 +6,19 @@
       <p>Software Engineering Student</p>
     </div>
     <div class="my-image">
+      <div class="short-contact d-flex flex-column align-center">
+        <div class="line" />
+        <div class="contact-container d-flex flex-column align-center justify-center">
+          <img
+            v-for="service in services"
+            :key="service.name"
+            alt="nav"
+            :src="imageSource(service.image)"
+            @click="contact(service.link)"
+          />
+        </div>
+        <div class="line" />
+      </div>
       <v-img
           src="@/assets/me.png"
           max-width="334"
@@ -17,7 +30,34 @@
 
 <script>
 export default {
-  name: 'Start'
+  name: 'Start',
+  data: () => ({
+    services: [
+      {
+        name: 'github',
+        image: 'github.png',
+        link: 'https://github.com/robineco',
+      },
+      {
+        name: 'LinkedIn',
+        image: 'linkedin.png',
+        link: 'https://www.linkedin.com/in/robin-niclas-schlund-946a87231',
+      },
+      {
+        name: 'Email',
+        image: 'email.png',
+        link: 'mailto:contact-robin@tutanota.de',
+      },
+    ],
+  }),
+  methods: {
+    imageSource(image) {
+      return require(`../../assets/contact/${image}`)
+    },
+    contact(link) {
+      window.location = link;
+    },
+  },
 }
 </script>
 
@@ -44,5 +84,25 @@ export default {
 }
 .v-image {
   border-radius: 80px 80px 0 0;
+}
+.short-contact {
+  position: absolute;
+  margin-top: 2rem;
+  margin-left: -10rem;
+  .contact-container {
+    width: 64px;
+    height: 180px;
+    background: #66B2F0;
+    border-radius: 16px;
+    gap: .25rem;
+    > img {
+      cursor: pointer;
+    }
+  }
+  .line {
+    width: 0;
+    height: 100px;
+    border: 2px solid #FFFFFF;
+  }
 }
 </style>
